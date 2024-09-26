77f26539df57fcd9cc90e1995d94026a3868ffe9
kuntal14 <sumankuntal6@gmail.com>
2024-09-26 10:02:48 +0530

    understanding compositions api

--- src/views/PagesList.vue
+++ src/views/PagesList.vue
@@ -1,3 +1,33 @@
 <template>
-    <p>this is the pages list</p>
+    <p>{{ data.counter }}</p>
+    <button @click.prevent="data.counter++">hola</button>
 </template>
+
+<script setup>
+
+import {ref, reactive} from 'vue';
+
+//const counter = ref(0);
+const data = reactive({counter:0});
+
+function click(){
+    //counter.value++;
+    data.counter++;
+}
+
+
+
+// export default {
+//     data(){
+//         return {
+//             counter : 0
+//         }
+//     },
+//     methods: {
+//         click(){
+//             this.counter++;
+//         }
+//     }
+// }
+
+</script>
